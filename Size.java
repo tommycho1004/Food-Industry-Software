959ac33fd12d8c7fb016f14cb31f82c890ba7bf3
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-11-11 21:51:31 -0500

    Update Size.java

--- Size.java
+++ Size.java
@@ -1,8 +1,16 @@
 package sample;
 
+/**
+ * The Size enum class that holds the three sizes of pizza.
+ * @author Tommy Cho, Neha Gudur
+ */
 public enum Size {
     Small, Medium, Large;
 
+    /**
+     * A method that returns the size of the pizza in String form
+     * @return size of Pizza in String form
+     */
     public String pizzaSizeString(){
         if (this == Small){
             return "Small";
