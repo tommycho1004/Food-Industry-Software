6846e5ec0481704db9245206f539963bb19ba8ad
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-11-11 21:49:46 -0500

    Update PizzaController.java

--- PizzaController.java
+++ PizzaController.java
@@ -1,5 +1,10 @@
 package sample;
 
+/**
+ * The controller class that holds all instance variables and methods for the GUI
+ *
+ * @author Tommy Cho, Neha Gudur
+ */
 public class PizzaController {
 
     private StoreOrders listOfOrders = new StoreOrders();
