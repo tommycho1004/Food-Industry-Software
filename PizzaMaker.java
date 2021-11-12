f115271e6a006b92650f37379d7c79f2b6e64e4e
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-11-11 21:52:35 -0500

    Update PizzaMaker.java

--- PizzaMaker.java
+++ PizzaMaker.java
@@ -3,6 +3,7 @@ package sample;
 /**
  * The PizzaMaker class that instantiates a pizza depending on a chosen flavor
  * Flavors are: Pepperoni, Hawaiian, Deluxe (case sensitive)
+ *
  * @author Tommy Cho, Neha Gudur
  */
 //create an instance of subclasses based on the chosen flavor
@@ -15,24 +16,23 @@ public class PizzaMaker {
 
     /**
      * A method that instantiates a pizza.
+     *
      * @param flavor flavor of pizza being created
      * @return pizza that was made
      */
     public static Pizza createPizza(String flavor) {
-        if (flavor.equals("Pepperoni") || flavor.equals("pepperoni")){
+        if (flavor.equals("Pepperoni") || flavor.equals("pepperoni")) {
             Pepperoni pizza = new Pepperoni();
             pizza.addTopping("Pepperoni");
             pizza.size = Size.Small;
             return pizza;
-        }
-        else if (flavor.equals("Hawaiian") || flavor.equals("hawaiian")){
+        } else if (flavor.equals("Hawaiian") || flavor.equals("hawaiian")) {
             Hawaiian pizza = new Hawaiian();
             pizza.addTopping("Ham");
             pizza.addTopping("Pineapple");
             pizza.size = Size.Small;
             return pizza;
-        }
-        else{ //if (flavor.equals("Deluxe") || flavor.equals("deluxe"))
+        } else { //if (flavor.equals("Deluxe") || flavor.equals("deluxe"))
             Deluxe pizza = new Deluxe();
             pizza.addTopping("Sausage");
             pizza.addTopping("Pepperoni");
