d452b260eaee47e610e1930a88b46e7add0fd0e0
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-11-16 20:07:13 -0500

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 
 
 /**
@@ -11,66 +13,17 @@ import java.util.ArrayList;
  */
 public class Order {
 
-    private String phoneNumber;
-    private ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
+    protected ObservableList<Pizza> pizzas;
+    String phoneNumber;
+    double orderTotal;
 
     /**
-     * No argument constructor for Order
+     * A default constructor for an order
      */
     public Order(){
-        //empty constructor
-    }
-
-    /**
-     * Constructor for Order with phonenumber
-     * @param phoneNumber phone number that relates to the order
-     */
-    public Order(String phoneNumber){
-        this.phoneNumber = phoneNumber;
-    }
-    /**
-     * Setter method for phone number
-     *
-     * @param phoneNumber number that is being set
-     */
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * Getter method for phone number
-     *
-     * @return phone number of instance
-     */
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-
-    /**
-     * Getter method for pizza list
-     * @return list of pizzas
-     */
-    public ArrayList<Pizza> getPizzas() {
-        return this.pizzas;
-    }
-
-    /**
-     * Method to add a pizza to the order
-     *
-     * @param pizza pizza being added
-     */
-    public void addPizza(Pizza pizza) {
-        pizzas.add(pizza);
-    }
-
-    /**
-     * Method to remove a pizza from the order
-     * The GUI will display the list of pizzas in the order, so the input can correspond to that.
-     *
-     * @param pizzaIndex index at which the pizza being removed is found
-     */
-    public void removePizza(int pizzaIndex) {
-            pizzas.remove(pizzaIndex);
+        this.pizzas = FXCollections.observableArrayList();
+        this.phoneNumber = "";
+        this.orderTotal = 0;
     }
 
     /**
@@ -113,42 +66,10 @@ public class Order {
         str.append("Order for " + phoneNumber + ".\n");
         str.append("Number of pizzas in this order: " + pizzas.size() + "\n");
         for (int i = 0; i < pizzas.size(); i++) {
-            str.append(i + indexFixer + ". " + pizzas.get(i).printPizza() + "\n");
+            str.append(i + indexFixer + ". " + pizzas.get(i) + "\n");
         }
         str.append("Tax: $" + dec.format(this.getTax()) + "\n");
         str.append("Order Total: $" + dec.format(this.getGrandTotal()) + "\n");
         return str.toString();
     }
-
-    /*public static void main(String[] args) { //testbed main
-        Order test = new Order(); //create new order
-        test.setPhoneNumber("7323369471"); //set phone number identifier
-        Pizza pizza1 = PizzaMaker.createPizza("Pepperoni"); //create new pepperoni pizza
-        System.out.println(pizza1.printPizza()); //default pizza
-        pizza1.addTopping("Pineapple"); //add pineapple topping
-        pizza1.addTopping("Pineapple"); //add pineapple topping (should return error msg)
-        System.out.println(pizza1.printPizza()); //print pepperoni pizza with pineapple
-        pizza1.increaseSize(); //increase size to medium
-        System.out.println(pizza1.printPizza()); //print medium pepperoni pizza w pineapple
-        pizza1.increaseSize(); //increase size to large
-        System.out.println(pizza1.printPizza()); //print large pepperoni pizza w pineapple
-        pizza1.decreaseSize(); //decrease size to medium
-        System.out.println(pizza1.printPizza()); //print medium pepperoni pizza w pineapple
-        pizza1.decreaseSize(); //decrease size to small
-        pizza1.decreaseSize(); //decrease size (should return error msg)
-        pizza1.removeTopping("Pineapple"); //remove pineapple
-        pizza1.removeTopping("Pineapple"); //remove pineapple (should return error msg)
-        pizza1.removeTopping("Pepperoni"); //remove pepperoni
-        System.out.println(pizza1.printPizza()); //print small pepperoni pizza without pepperoni but same base price
-        test.addPizza(pizza1); //add pizza1 to order
-        Pizza pizza2 = PizzaMaker.createPizza("Hawaiian"); //create new pizza
-        Pizza pizza3 = PizzaMaker.createPizza("Deluxe"); //create new pizza
-        pizza2.increaseSize(); //increase size of pizza2 to medium
-        pizza3.addTopping("Pineapple"); //add pineapple to pizza3
-        test.addPizza(pizza2); //add pizza2 to order
-        test.addPizza(pizza3); //add pizza3 to order
-        System.out.println(test.printOrder()); //print order
-        test.removePizza(2); //remove pizza3 from order
-        System.out.println(test.printOrder()); //print order
-    }*/
 }
